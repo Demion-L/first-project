53d23e54552dacec78a3822a9ed286fc1cfbdb0a
Dmitriy Latysh <pradovds@ukr.net>
2021-01-07 17:17:12 +0200

    Work with forms added

--- js/script.js
+++ js/script.js
@@ -126,7 +126,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  //const modalTimerId = setTimeout(openModal, 20000);
+  const modalTimerId = setTimeout(openModal, 200000);
 
   function showModalByScroll() {
     if (
@@ -209,6 +209,57 @@ window.addEventListener("DOMContentLoaded", () => {
     ".menu .container"
   ).render();
 
+//Forms
+const forms = document.querySelectorAll('form');
+
+const message = {
+  loading: "Загрузка",
+  succes: "Спасибо! За вами уже выехали!",
+  failure: "Упс! Что-то пошло не так!"
+};
+
+forms.forEach(item => {
+  postData(item);
+});
+
+function postData(form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const statusMessage = document.createElement('div');
+    statusMessage.classList.add('status');
+    statusMessage.textContent = message.loading;
+    form.append(statusMessage);
+
+    const request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+
+    request.setRequestHeader('Content-type', 'aplication/json');
+    const formData = new FormData(form);
+
+    const object = {};
+    formData.forEach(function (value, key) {
+      object[key] = value;
+    });
+
+    const json = JSON.stringify(object);
+
+    request.send(json);
+
+    request.addEventListener('load', () => {
+      if (request.status === 200) {
+        console.log (request.response);
+        statusMessage.textContent = message.succes;
+        form.reset();
+        setTimeout(() => {
+          statusMessage.remove();
+        }, 3000);
+      } else {
+        statusMessage.textContent = message.failure;
+      }
+    });
+  });
+}
 
 
 });
